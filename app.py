098054c0dec58cbbbdf21ced75243ed49764cd97
CharlesKibet <charlesbiegon973@gmail.com>
2024-07-30 18:49:45 +0300

    Modeling the database

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
+#Making the views
 from flask import Flask
+from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from models import db
 
-
-#App initialization
+# App initialization
 app = Flask(__name__)
+api = Api(app)
 
+
+# Config the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///boda.db'
+
+
+CORS(app)
+migrate = Migrate(app,db,render_as_batch=True)
+
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+
+# Set up of views
+
+# api.add_resource(Register, '/register')
+# api.add_resource(Login, '/login')
 @app.route('/')
 def home():
-  return '<h1>Hey from flask</h1>'
-
-
-
-
+    return '<h1>Hey from flask</h1>'
--- models.py
+++ models.py
@@ -1,1 +1,51 @@
+# Database designing
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
+
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
+
+# Models
+
+
+class Rider(db.Model, SerializerMixin):
+    # Our table  to store the data for our riders
+    __tablename__ = 'riders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    phone = db.Column(db.Text, nullable=False, unique=True)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    license = db.Column(db.Text, nullable=False, unique=True)
+    certificate = db.Column(db.Text, nullable=False, unique=True)
+    insurance = db.Column(db.Text, nullable=False, unique=True)
+    residence = db.Column(db.Text, nullable=False)
+
+
+class Lawyer(db.Model, SerializerMixin):
+
+    # Our table to store the data for our lawyers
+    __tablename__ = 'lawyers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    phone = db.Column(db.Text, nullable=False, unique=True)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    degree = db.Column(db.Text, nullable=False)
+    residence =db.Column(db.Text,nullable=False)
